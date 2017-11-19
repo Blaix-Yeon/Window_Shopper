c40ec949a8e09d99dd1de9b117aebb0608bb2692
Blaix <blaid@blaixui-MacBook-Pro.local>
2017-11-20 02:23:11 +0900

    third

--- Window_Shopper/Window_Shopper/VIews/CurrencyTxtField.swift
+++ Window_Shopper/Window_Shopper/VIews/CurrencyTxtField.swift
@@ -12,6 +12,21 @@ import UIKit
 
 class CurrencyTxtField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size:CGFloat = 20
+        let currencyLbl = UILabel(frame: CGRect(x: 5, y: (frame.size.height / 2) - size / 2, width: size, height: size))
+        currencyLbl.backgroundColor = #colorLiteral(red: 0.9081935287, green: 0.9027946591, blue: 0.9123434424, alpha: 0.7969017551)
+        currencyLbl.textAlignment = .center
+        currencyLbl.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLbl.layer.cornerRadius = 5.0
+        currencyLbl.clipsToBounds = true
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        currencyLbl.text = formatter.currencySymbol
+        addSubview(currencyLbl)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -26,6 +41,7 @@ class CurrencyTxtField: UITextField {
         layer.cornerRadius = 5.0
         textAlignment = .center
         
+        clipsToBounds = true
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
